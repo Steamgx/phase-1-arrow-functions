c8bd60b398a63cfa4632ef71e6a1222727812995
Steamgx <your-bmuraya646@gmail.com>
2024-12-12 08:14:55 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function divide() {
+    let a = 2000;
+    let b = 100;
+    let result = a / b; // Assuming division, not modulus, was intended
+    return result;
+}
+const square =(param1)=>param1*param1;
+square(4);
+
+const add=(par1,par2)=>par1+par2
+add(2,3);
